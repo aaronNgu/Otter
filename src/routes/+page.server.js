fc74832c478d9df9131536b405326ddb7534d1de
aaron_other <aaron_other@airwork.local>
2023-06-26 22:22:47 -0700

    added actions for todo list

---
+++ src/routes/+page.server.js
@@ -0,0 +1,50 @@
+import { todos } from "./data";
+
+let faketodos = todos;
+let nextId = 4;
+
+export function load() {
+    return {
+        todos: faketodos.map((todo) => ({
+            id: todo.id,
+            name: todo.name,
+        }))
+    }
+}
+
+/**
+ * @type {import('./$types').Actions}
+ */
+export const actions = {
+    add_todo: async ({ request }) => {
+        let data = await request.formData();
+        let name = data.get('name');
+        if (!name) { throw new Error('name cannot be empty') };
+        faketodos.push({ id: nextId, name: name.toString() });
+        nextId += 1;
+        return {
+            successful: true
+        }
+    },
+    delete_todo: async ({ request }) => {
+        let data = await request.formData();
+        let id = data.get('id');
+        if (!id) { throw new Error('id cannot be empty') };
+        faketodos = faketodos.filter((todo) => todo.id != Number(id ?? 0));
+        return {
+            successful: true
+        }
+    },
+    update_todo: async ({ request }) => {
+        let data = await request.formData();
+        let id = data.get('id');
+        let name = data.get('name');
+        if (!id || !name) { throw new Error('name cannot be empty') };
+        let to_update = faketodos.find((ele) => ele.id == Number(id ?? 0));
+        if (!to_update) throw new Error('item not found');
+        to_update.name = name?.toString();
+        return {
+            successful: true
+        }
+    }
+}
