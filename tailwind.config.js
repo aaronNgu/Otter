29e06affb87146da0dbb7dca3d26561643a66981
aaron_other <aaron_other@airwork.local>
2023-06-26 22:20:12 -0700

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      'pri': '#E7CEA6',
+      'sec': '#0A6EBD',
+      'sec_2': '#5A96E3',
+      'sec_3': '#A1C2F1',
+    },
+  },
+  plugins: [],
+}
+
